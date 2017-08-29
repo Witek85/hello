85301ed9d605302caee681ff97df45614599d87e
Witek85 <witold.szklarski@gmail.com>
2017-08-29 22:52:47 +0200

    init render

--- server.js
+++ server.js
@@ -25,20 +25,25 @@ app.get("/",function(req,res){
 /*  This is auto initiated event when Client connects to Your Machien.  */
 
 io.on('connection',function(socket){  
-    console.log("A user is connected");
-    socket.on('status added',function(status){
-      add_status(status,function(res){
-        if(res){
-            io.emit('refresh feed',status);
-        } else {
-            io.emit('error');
-        }
+
+  pool.query('SELECT `s_text` FROM fbstatus', function (error, results, fields) {
+    if (error) throw error;
+    io.emit('init feed',results);
+  });
+
+  socket.on('status added',function(status){
+    add_status(status,function(res){
+      if(res){
+        io.emit('refresh feed',status);
+      } else {
+        io.emit('error');
+      }
       });
     });
 });
 
 var add_status = function (status,callback) {
-  console.log('addstatus');
+
     pool.getConnection(function(err,connection){
         if (err) {
           callback(false);
